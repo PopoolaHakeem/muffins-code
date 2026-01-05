b81f3d906aef2d65a392461f29997932e9936134
Hakeem Popoola <Popoola HAkeem>
2026-01-05 12:23:39 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,14 @@
+const mobileMenu = document.getElementById("mobile-menu");
+const openMenuButtons = document.querySelectorAll("[command='open']");
+const closeMenuButtons = document.querySelectorAll("[command='close']");
+openMenuButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    mobileMenu.showModal();
+  });
+});
+
+closeMenuButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    mobileMenu.close();
+  });
+});
