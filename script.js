9127eba1a906448919acab3e328f10ec34c9b1f8
Hakeem Popoola <Popoola HAkeem>
2026-01-16 11:39:43 +0100

    my changes

--- script.js
+++ script.js
@@ -2,30 +2,38 @@ const mobileMenu = document.querySelector('#mobile-menu');
 const openMenuButton = document.querySelector('#openMenu');
 const closeMenuButton = document.querySelector('#closeMenu');
 
-const nav = document.querySelectorAll("nav");
-nav.addEventListener('scrollY', () => {
-  if (window.scrollY > 1) {
-    nav.classList.add('bg-#000', 'shadow-lg');
-  } else {
-    nav.classList.remove('bg-black', 'shadow-lg');
-  }
-});
-
 openMenuButton.addEventListener('click', () => {
   mobileMenu.classList.remove('hidden');
   openMenuButton.classList.add('hidden');
 });
+
 closeMenuButton.addEventListener('click', () => {
   mobileMenu.classList.add('hidden');
-    openMenuButton.classList.remove('hidden');
+  openMenuButton.classList.remove('hidden');
 });
 
-for (const link of mobileMenu.querySelectorAll('a')) {
-  link.addEventListener('click', () => {
-    mobileMenu.classList.add('hidden');
-    openMenuButton.classList.remove('hidden');
-  });
-}
+// for (const link of mobileMenu.querySelectorAll('a')) {
+//   link.addEventListener('click', () => {
+//     mobileMenu.classList.add('hidden');
+//     openMenuButton.classList.remove('hidden');
+//   });
+// }
+
+
+
+
+
+
+
+
+// const nav = document.querySelectorAll("nav");
+// nav.addEventListener('scrollY', () => {
+//   if (window.scrollY > 1) {
+//     nav.classList.add('bg-#000', 'shadow-lg');
+//   } else {
+//     nav.classList.remove('bg-black', 'shadow-lg');
+//   }
+// });
 
 // nav.addEventListener('scrollY', () => {
 //   if (window.scrollY > 1) {
